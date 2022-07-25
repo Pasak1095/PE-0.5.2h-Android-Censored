72c4e85f0edb85f15ed0146fd857374e100832f0
Pasak1095 <109857769+Pasak1095@users.noreply.github.com>
2022-07-25 14:28:45 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,8 +35,8 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		//#if MODS_ALLOWED 'mods', #end
+		//#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		#if !switch 'donate', #end
 		'options'
@@ -123,7 +123,31 @@ class MainMenuState extends MusicBeatState
 			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
 			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
 			menuItem.updateHitbox();
-		}
+		        switch (i) 
+                        {
+                            case 0:
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                            case 1:
+                                menuItem.x -= 300; 
+                                menuItem.y -= 100;
+                            case 2: 
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                            case 3:
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                            case 4:
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                            case 5:
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                            case 6:
+                                menuItem.x -= 300;
+                                menuItem.y -= 100;
+                       }
+                }
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
@@ -235,19 +259,19 @@ class MainMenuState extends MusicBeatState
 
 								switch (daChoice)
 								{
-									case 'story_mode':
+									case 'Story_Mode':
 										MusicBeatState.switchState(new StoryMenuState());
-									case 'freeplay':
+									case 'Freeplay':
 										MusicBeatState.switchState(new FreeplayState());
 									#if MODS_ALLOWED
-									case 'mods':
+									case 'Mods':
 										MusicBeatState.switchState(new ModsMenuState());
 									#end
-									case 'awards':
+									case 'Awards':
 										MusicBeatState.switchState(new AchievementsMenuState());
-									case 'credits':
+									case 'Credits':
 										MusicBeatState.switchState(new CreditsState());
-									case 'options':
+									case 'Options':
 										LoadingState.loadAndSwitchState(new options.OptionsState());
 								}
 							});
